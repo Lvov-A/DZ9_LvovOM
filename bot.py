9f7a8425a66fd1b888330100613a29700146d749
Lvov Aleksey <lvovlvov92@gmail.com>
2022-12-07 23:54:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,16 @@ list_user = {
     "Sergo": "0986666666",
 }
 
-def parser():
-    msg = input("Enter command:")
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as exception:
+            return exception.args[0]
+    return wrapper
+
+
+def parser(msg):
     msg = msg.replace("show all", "show_all")
     msg = msg.replace("good bye", "good_bye")
     msg = msg.split(' ')
@@ -21,18 +29,27 @@ def parser():
     return command, name, phone
 
 def show_all():
+    list_contacts = ""
     for key, value in list_user.items():
-        print(key, value)
+        list_contacts += f'{key} : {value} \n'
+    return list_contacts[:-2]
 
+@input_error
 def show_phone(name):
     res_phone = list_user.get(name)
-    print(res_phone)
+    if res_phone == None:
+        raise ValueError(f"Contact {name} not found")
+    else:
+        return res_phone
 
+@input_error
 def add_contact(name, phone):
     if not name or not phone:
-        print("More arguments needed")
+        raise ValueError("More arguments needed")
     else:
         list_user[name] = phone
+        return "Number saved"
+
 
 OPERATIONS = {
     "show all": show_all,
@@ -41,22 +58,30 @@ OPERATIONS = {
     "change": add_contact
 }
 
-def handler():
+def handler(user_msg):
+    list_msg = parser(user_msg)
+    if list_msg[0] == "hello":
+        return "How can I help you?"   
+    elif list_msg[0] == "add":
+        return(OPERATIONS["add"](list_msg[1], list_msg[2]))
+    elif list_msg[0] == "change":
+        return(OPERATIONS["change"](list_msg[1], list_msg[2]))        
+    elif list_msg[0] == "phone":
+        return(OPERATIONS["phone"](list_msg[1]))  
+    elif list_msg[0] == "show_all":
+        return(OPERATIONS["show all"]())    
+    elif list_msg[0] == "good_bye" or list_msg[0] == "close" or list_msg[0] == "exit":
+        return "Good bye!"
+    else:
+        return "Error command"
+
+def main():
     while True:
-        list_msg = parser()
-        if list_msg[0] == "hello":
-            print("How can I help you?")   
-        elif list_msg[0] == "add":
-            OPERATIONS["add"](list_msg[1], list_msg[2]) 
-        elif list_msg[0] == "change":
-            OPERATIONS["change"](list_msg[1], list_msg[2])        
-        elif list_msg[0] == "phone":
-            OPERATIONS["phone"](list_msg[1])  
-        elif list_msg[0] == "show_all":
-            OPERATIONS["show all"]()       
-        elif list_msg[0] == "good_bye" or list_msg[0] == "close" or list_msg[0] == "exit":
-            print("Good bye!")
+        user_msg = input("Enter command:")
+        result = handler(user_msg)
+        print(result)
+        if result == "Good bye!":
             break
 
 if __name__ == "__main__":
-    handler()
+    main()
